440c02e948579a536390c0f51b1845e7d4895337
xgl34222220-ops <xgl34222220@gmail.com>
2026-08-03 00:41:47 +0800

    build: bump Tianji to v5.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.tianji.probabilitylab.nativev4"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "5.4.7"
+        versionCode = 25
+        versionName = "5.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
