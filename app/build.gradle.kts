7c977ca2fd7168f789edeb4b7d7b5afe5834b8b0
xgl34222220-ops <xgl34222220@gmail.com>
2026-08-06 06:56:03 +0800

    release: bump app version to 6.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.tianji.probabilitylab.nativev5"
         minSdk = 26
         targetSdk = 36
-        versionCode = 57
-        versionName = "5.10.5"
+        versionCode = 58
+        versionName = "6.0.0"
 
         buildConfigField("String", "TIANJI_CLOUD_BASE_URL", "\"${cloudBaseUrl.get()}\"")
         buildConfigField("String", "TIANJI_FIREBASE_PROJECT_ID", "\"${firebaseProjectId.get()}\"")
