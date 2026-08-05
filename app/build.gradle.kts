88144c27091cb4d4e9908a0a175d09cdadc9c7b6
xgl34222220-ops <xgl34222220@gmail.com>
2026-08-05 14:33:45 +0800

    chore: bump Tianji to v5.9.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.tianji.probabilitylab.nativev5"
         minSdk = 26
         targetSdk = 36
-        versionCode = 48
-        versionName = "5.9.6"
+        versionCode = 49
+        versionName = "5.9.7"
 
         buildConfigField("String", "TIANJI_CLOUD_BASE_URL", "\"${cloudBaseUrl.get()}\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
