01f2303fc2d2b2bafed4ad3546ef2105595dc8cb
xgl34222220-ops <xgl34222220@gmail.com>
2026-08-06 13:57:55 +0800

    chore(release): 发布 v6.1.0 正式版

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.tianji.probabilitylab.nativev5"
         minSdk = 26
         targetSdk = 36
-        versionCode = 61
-        versionName = "6.1.0-beta03"
+        versionCode = 62
+        versionName = "6.1.0"
 
         buildConfigField("String", "TIANJI_CLOUD_BASE_URL", "\"${cloudBaseUrl.get()}\"")
         buildConfigField("String", "TIANJI_FIREBASE_PROJECT_ID", "\"${firebaseProjectId.get()}\"")
