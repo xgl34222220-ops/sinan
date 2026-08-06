fb57ad73e5aee9a21566e3d0041dff8cba1c4dcb
xgl34222220-ops <xgl34222220@gmail.com>
2026-08-06 22:31:33 +0800

    feat: install AI continual-learning calibration

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -4,3 +4,8 @@ from .runtime_patches import install as _install_runtime_patches
 
 _install_runtime_patches()
 del _install_runtime_patches
+
+from .ai_continual_bridge import install as _install_ai_continual_bridge
+
+_install_ai_continual_bridge()
+del _install_ai_continual_bridge
