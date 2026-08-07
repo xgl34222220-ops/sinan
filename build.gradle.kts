c734e0c3e70a64f08ad1d3c067ceb33989d30111
xgl34222220-ops <xgl34222220@gmail.com>
2026-08-07 13:01:25 +0800

    test: enable Compose preview screenshot plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
+    id("com.android.compose.screenshot") version "0.0.1-alpha15" apply false
 }
