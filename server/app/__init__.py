e22a8eefb6365365f955d438c0650e27f942c43f
xgl34222220-ops <xgl34222220@gmail.com>
2026-08-07 08:18:27 +0800

    feat: enable fixed 235780 position predictor

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -9,3 +9,8 @@ from .ai_continual_bridge import install as _install_ai_continual_bridge
 
 _install_ai_continual_bridge()
 del _install_ai_continual_bridge
+
+from .fixed_target_bridge import install as _install_fixed_target_bridge
+
+_install_fixed_target_bridge()
+del _install_fixed_target_bridge
